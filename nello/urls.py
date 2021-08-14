e93a5e4241a9143d17c23011240b4c63d020bac3
Dev-Sherlock <khanbayov.tamerlan@gmail.com>
2021-08-14 18:23:02 +0200

    login page template and url added.

--- nello/urls.py
+++ nello/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('parentnello.urls'))
+    path('', include('parentnello.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
